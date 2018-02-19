4b0d8669abd027c1ccf455fa7cf4b596a0bb6e54
ripo <ripohassan5@gmail.com>
2018-02-19 22:26:49 +0600

    initial commit

--- app/src/main/java/ripo/com/jsonparsing/MainActivity.java
+++ app/src/main/java/ripo/com/jsonparsing/MainActivity.java
@@ -1,13 +1,22 @@
 package ripo.com.jsonparsing;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,14 +28,43 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ArrayList<JSONResponse> data = new ArrayList<>();
     private DataAdapter adapter;
+    LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Snackbar snackbar = Snackbar
+                .make(layout, "No Internet Connection!", Snackbar.LENGTH_LONG)
+                .setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        WifiManager manager= (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                        manager.setWifiEnabled(true);
+//                        Intent emailIntent = new Intent(Intent.ACTION_MANAGE_NETWORK_USAGE);
+//                        emailIntent.setData(Uri.parse(""));
+//                        emailIntent.setType("text/plain");
+                    }
+                });
+// Changing message text color
+        snackbar.setActionTextColor(Color.RED);
+// Changing action button text color
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.YELLOW);
+        snackbar.show();
+
+    }
+
     private void initViews(){
-        recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+        layout= findViewById(R.id.linier) ;
+        recyclerView =findViewById(R.id.card_recycler_view);
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -55,4 +93,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
